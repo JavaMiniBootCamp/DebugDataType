06fd48ec3d1c83df64670f5e48a4827e5eae51cf
SienTien <hsientien0212@gmail.com>
2020-08-24 20:13:52 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int number1=10;
+        int number2 = 1;
+        int number3= 15 ;
+        int product;
+        int sum;
+        String Product="Product ";
+        String sumString ="Sum ";
+
+        product = number1*number2*number3;
+        sum = number1+number1+number3;
+        System.out.println(Product+product+"\n"+sumString+sum);
+    }
+}
